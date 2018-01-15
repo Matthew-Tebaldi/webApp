4eea4b622a2f58ea7fa9abb0c86cf6861dc1b4a4
Matthew Tebaldi <Tebaldi@Matthews-MBP.local>
2018-01-15 10:28:03 -0500

    made outline

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressValidator = require('express-validator');
+var mongojs = require('mongojs');
+var db = mongojs('customerapp', ['users']);
+var ObjectId = mongojs.ObjectId; 
+
+var app = express();
+
+console.log('worked');
+
+
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+
+app.get('/', function (req, res) {
+   
+        res.render('index', {
+            title: 'Customers',
+            users: 'Users;'
+    })
+});
+
+
+
+app.listen(3000, function () {
+    console.log('Server started on Port 3000');
+})
